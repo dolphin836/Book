9ecdb5e4545e8c85f1a012657b8d6cbb4b98c48a
wanghaibing <wanghaibing836@gmail.com>
2020-06-30 14:36:44 +0800

    新增自动生成脚本

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ## 技术类
 | 书 名 | 格 式 | 大 小 | 操 作 |
 | ---- | ---- | ---- | ---- |
-| RabbitMQ 实战指南 | PDF | 4.72 MB | [![](download.png)](https://github.com/dolphin836/Book/raw/master/%E6%8A%80%E6%9C%AF%E7%B1%BB/RabbitMQ%20%E5%AE%9E%E6%88%98%E6%8C%87%E5%8D%97.pdf) |
+| Java开发手册 | PDF | 1.44 mb | [下载](https%3A%2F%2Fgithub.com%2Fdolphin836%2FBook%2Fraw%2Fmaster%2F%E6%8A%80%E6%9C%AF%E7%B1%BB%2FJava%E5%BC%80%E5%8F%91%E6%89%8B%E5%86%8C.pdf) |
+| RabbitMQ 实战指南 | PDF | 4.73 mb | [下载](https%3A%2F%2Fgithub.com%2Fdolphin836%2FBook%2Fraw%2Fmaster%2F%E6%8A%80%E6%9C%AF%E7%B1%BB%2FRabbitMQ+%E5%AE%9E%E6%88%98%E6%8C%87%E5%8D%97.pdf) |
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+// 根据文件目录生成 README.md 文件
+// 遍历文件夹
+function found ($dir)
+{
+    // 跳过 Git 目录
+    if ($dir === '.\.git') {
+        return;
+    }
+
+    if ($dir !== '.') {
+        $category = substr($dir, 2);
+    
+        global $text;
+    
+        $text .= '## ' . $category . PHP_EOL;
+        $text .= '| 书 名 | 格 式 | 大 小 | 操 作 |' . PHP_EOL;
+        $text .= '| ---- | ---- | ---- | ---- |' . PHP_EOL;
+    }
+
+    $results  = new \FilesystemIterator($dir);
+
+    foreach ($results as $result) {
+        // 路径
+        $path = $result->getPath();
+        // 跳过根目录下的文件
+        if ($path === '.' && $result->isFile()) {
+            continue;
+        }
+        // 递归目录
+        if ($result->isDir()) {
+            found($result->getPathname());
+        }
+        // 过滤
+        if (! $result->isFile()) continue;
+
+        $fullname = $result->getFilename();
+        $size     = $result->getSize();
+        $exen     = $result->getExtension();
+        $name     = substr($fullname, 0, strlen ($fullname) - strlen ($exen) - 1);
+        $http     = 'https://github.com/dolphin836/Book/raw/master/' . $category . '/' . $fullname;
+        $text    .= '| ' . $name . ' | ' . strtoupper($exen) . ' | ' . size($size) . ' | [下载](' . urlencode($http) . ') |' . PHP_EOL;
+        
+        echo  $name . PHP_EOL;
+    }
+}
+
+// 计算文件大小
+function size ($size)
+{
+    $k = round($size / 1024, 2);
+
+    if ($k < 1024) {
+        return $k . ' kb';
+    }
+
+    $m = round($size / 1024 / 1024, 2);
+
+    return $m . ' mb';
+}
+
+$head = '# 目录' . PHP_EOL;
+
+found('.');
+
+file_put_contents('README.md', $head . $text);
