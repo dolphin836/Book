a000015d78dde57fb291630cbac4a1a3c53dc404
wanghaibing <wanghaibing836@gmail.com>
2020-06-30 14:39:20 +0800

    some

--- index.php
+++ index.php
@@ -38,8 +38,8 @@ function found ($dir)
         $size     = $result->getSize();
         $exen     = $result->getExtension();
         $name     = substr($fullname, 0, strlen ($fullname) - strlen ($exen) - 1);
-        $http     = 'https://github.com/dolphin836/Book/raw/master/' . $category . '/' . $fullname;
-        $text    .= '| ' . $name . ' | ' . strtoupper($exen) . ' | ' . size($size) . ' | [下载](' . urlencode($http) . ') |' . PHP_EOL;
+        $http     = 'https://github.com/dolphin836/Book/raw/master/' . urlencode($category) . '/' . urlencode($fullname);
+        $text    .= '| ' . $name . ' | ' . strtoupper($exen) . ' | ' . size($size) . ' | [下载](' . $http . ') |' . PHP_EOL;
         
         echo  $name . PHP_EOL;
     }
